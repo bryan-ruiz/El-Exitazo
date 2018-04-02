e19589f7b89f36bf9423d392aa12d9062b06e41f
bryan-ruiz <bryanrb95@gmail.com>
2018-04-02 00:31:52 -0600

    Globales
    para controlar selling

---
+++ Exitazo Costa Rica/ExitazoCostaRica/app/Globals.js
@@ -0,0 +1,19 @@
+var billList = [];
+var currentBill = 0;
+
+function createBill() {
+    var product = new Object();
+    product.
+}
+
+function deleteBill() {
+
+}
+
+function getBillList() {
+
+}
+
+function getCurrentBill() {
+
+}
